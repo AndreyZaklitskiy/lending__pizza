a074e11d6d5600807a60b3d20fa943d5142c94d9
AndreyZaklitskiy <glockman94@gmail.com>
2019-01-17 21:12:48 +0200

    snacks-section

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -44,13 +44,42 @@ $(document).ready(function(){
   });
 
   //слайдер с закусками
-  $('.snack-slider').slick({
+  $('.snacks-slider').slick({
     infinite: true,
     slidesToShow: 4,
     slidesToScroll: 2,
     dots: true,
     prevArrow: '<button type="button" class="slick-prev"><i class="fas fa-chevron-left"></i></button>',
-    nextArrow: '<button type="button" class="slick-next"><i class="fas fa-chevron-right"></i></button>'
+    nextArrow: '<button type="button" class="slick-next"><i class="fas fa-chevron-right"></i></button>',
+    responsive: [
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 720,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+        }
+      }
+
+    ]
   });
 
   //cлайдер с напитками
