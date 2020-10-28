c779762542700f21a92045e7495981a50eb57d02
AndreyZaklitskiy <glockman94@gmail.com>
2020-10-28 23:41:50 +0200

    work on offers-section

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -6,7 +6,7 @@ $(document).ready(function(){
   });
 
   //Верхний слайдер (горячие предложения).
-  $('.top-slider').slick({
+  $('.offers-slider').slick({
     prevArrow: '<button type="button" class="slick-prev"><i class="fas fa-chevron-left"></i></button>',
     nextArrow: '<button type="button" class="slick-next"><i class="fas fa-chevron-right"></i></button>'
 });
